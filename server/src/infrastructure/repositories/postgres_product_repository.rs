205d038cd9301ea2efe055ffb94bde80c14ef8ed
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2024-12-03 19:50:42 +0700

    server: add PostgreSQL database connection and repository structure

---
+++ server/src/infrastructure/repositories/postgres_product_repository.rs
@@ -0,0 +1,14 @@
+use crate::infrastructure::db::connection::DBPool;
+
+#[derive(Clone)]
+pub struct PostgresProductRepository {
+    pool: DBPool,
+}
+
+impl PostgresProductRepository {
+    pub fn new(pool: DBPool) -> Self {
+        PostgresProductRepository {
+            pool
+        }
+    }
+}
--- server/src/infrastructure/web.rs
+++ server/src/infrastructure/web.rs
@@ -2,14 +2,23 @@ use std::env;
 use actix_web::{web, App, HttpServer};
 use actix_web::middleware::Logger;
 use log::info;
+use crate::infrastructure::db::connection::establish_connection;
+use crate::infrastructure::repositories::postgres_product_repository::PostgresProductRepository;
 
 pub async fn run() -> std::io::Result<()> {
   let port = env::var("PORT").expect("PORT must be set");
+  let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+  let pool = establish_connection(&database_url);
+
+  let product_repo = PostgresProductRepository::new(pool.clone());
+
+  let app_data = web::Data::new(product_repo);
 
   info!("Starting...!");
 
   HttpServer::new(move || {
     App::new()
+      .app_data(app_data.clone())
       .wrap(Logger::default())
       .route("/", web::get().to(|| async { "E-Commerce API" }))
   })
